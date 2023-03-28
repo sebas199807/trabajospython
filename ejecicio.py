ca12bbc6bec08d4798d4ce32104469138631aa97
sebas199807 <svc19980702@gmail.com>
2023-03-28 09:59:17 -0500

    v6

---
+++ ejecicio.py
@@ -0,0 +1,10 @@
+nombre="ARIEL SEBASTIAN"
+apellido="VILLA CRESPO"
+print(nombre,apellido)
+
+#CONVERTIR DE DATO A TEXTO
+
+numero=int(input("INGRESE EL NUMERO"))
+print(type(numero))
+cadena=str(numero)
+print(type(cadena))
--- hola.py
+++
@@ -1,2 +0,0 @@
-print ("HOLA MUNDO")
-print ("HOLA")
