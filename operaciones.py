b423bd9253b9d58c024f2f7ff12ba5dde7ad3dd2
sebas199807 <svc19980702@gmail.com>
2023-03-28 08:30:08 -0500

    V5

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+dato1 = int(input("Ingrese numero 1"))
+dato2 = int(input("Ingrese numero 2"))
+sum= dato1 + dato2
+print ("La suma es",sum)
+
+resta = dato1 - dato2
+print ("La resta es",resta)
+
+multi = dato1 * dato2
+print ("La multiplicacion es",multi)
+
+div = dato1/dato2
+print ("La division es",div)
---
+++ triangulo.py
@@ -0,0 +1,4 @@
+base = float(input("Ingrese la base"))
+altura = float(input("Ingrse la altura"))
+area =  (base * altura)/2
+print ("El area es ",area)
