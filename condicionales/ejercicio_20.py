6442d84492607deab323c044d73301e0b56a6f76
sebas199807 <svc19980702@gmail.com>
2023-04-02 23:32:50 -0500

    vx

---
+++ condicionales/ejercicio_20.py
@@ -0,0 +1,9 @@
+dia = int(input("Ingrese Cantidad de Dias : "))
+
+hor = int(dia*24)
+min = int(hor*60)
+seg = int(dia * 86400)
+
+print("HORAS   : ", hor)
+print("MINUTOS : ", min)
+print("SEGUNDOS: ", seg)
